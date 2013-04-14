489306abbfd761050342a14e94f2a61bc386ea2e
Phantomas <manolis837@gmail.com>
2013-04-14 19:03:22 +0300

    trivial

--- search.php
+++ search.php
@@ -14,8 +14,9 @@ $con=mysql_connect("localhost","root","");
 
 
 $result = mysql_query("SELECT * FROM contacts WHERE $n ='$m'",$con);
+$number=mysql_num_rows($result);
 
-while($row = mysql_fetch_array($result))
+while($row = mysql_fetch_array($number))
   {
   echo $row['id'] ." ". $row['name']." ".$row['surname']." ".$row['phone']." ". $row['email'];
   echo "<br>";
