9c352d57b0595d86fbd8999fbcf0d0d41a1e81e7
Phantomas <manolis837@gmail.com>
2013-04-14 18:32:14 +0300

    TRIVIAL

--- search.php
+++ search.php
@@ -11,14 +11,11 @@ $n=$_POST["input1"];
 
 $con=mysql_connect("localhost","root","");
 // Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
 
-$result = mysqli_query($con,"SELECT * FROM contacts WHERE $n ='$m'");
 
-while($row = mysqli_fetch_array($result))
+$result = mysql_query($con,"SELECT * FROM contacts WHERE $n ='$m'");
+
+while($row = mysql_fetch_array($result))
   {
   echo $row['id'] ." ". $row['name']." ".$row['surname']." ".$row['phone']." ". $row['email'];
   echo "<br>";
